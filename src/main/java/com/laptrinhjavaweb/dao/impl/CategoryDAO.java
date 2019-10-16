30a8c50163bf4e2cfd280d2ad6082d082b9ada39
phuc <hoangphuc735@gmail.com>
2019-10-17 00:05:07 +0700

    update model DAO

--- src/main/java/com/laptrinhjavaweb/dao/impl/CategoryDAO.java
+++ src/main/java/com/laptrinhjavaweb/dao/impl/CategoryDAO.java
@@ -2,7 +2,10 @@ package com.laptrinhjavaweb.dao.impl;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.laptrinhjavaweb.dao.ICategoryDAO;
@@ -13,7 +16,7 @@ public class CategoryDAO implements ICategoryDAO{
 	public Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/test";
+			String url = "jdbc:mysql://127.0.0.1:3306/jspservletjdbc";
 			String user = "root";
 			String password= "1234";
 			
@@ -27,8 +30,46 @@ public class CategoryDAO implements ICategoryDAO{
 	
 	@Override
 	public List<CategoryModel> findAll() {
-		// TODO Auto-generated method stub
+		List<CategoryModel> result = new ArrayList<>();
+		String sql = "SELECT * FROM category";
+		//open connection
+		Connection connection = getConnection();
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		if (connection != null) {
+			try {
+				statement = connection.prepareStatement(sql);
+				resultSet = statement.executeQuery();
+				while (resultSet.next()) {
+					CategoryModel category = new CategoryModel();
+					category.setId(resultSet.getLong("id"));
+					category.setCode(resultSet.getString("code"));
+					category.setName(resultSet.getString("name"));
+					result.add(category);
+				}
+				return result;
+			} catch (SQLException e) {
+				return null;
+			} finally {
+				try {
+					if (connection != null) {
+						connection.close();
+					}
+					if (statement != null) {
+						statement.close();
+					}
+					if (resultSet != null) {
+						resultSet.close();
+					}
+				}catch (SQLException e) {
+					return null;
+				}
+				
+			}
+		}
+		
 		return null;
+		
 	}
 	
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,2 +1,2 @@
-<%@include file="/common/taglib.jsp"%>
-<c:redirect url="/trang-chu"/>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<c:redirect url = "/trang-chu"/>
